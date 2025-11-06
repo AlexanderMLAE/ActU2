3f53203ddd973e6b95982b2a4bb6cc60971a2801
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2025-11-06 07:40:58 -0500

    escribir y leer lista

--- randoms.py
+++ randoms.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 mylist = []
 
@@ -6,8 +7,16 @@ for i in range (0,10000):
     a = random.uniform(1000, 9999)
     mylist.append(float(f"{a:.2f}"))
 
-print(mylist)
+#print(mylist)
 
+try:
+    with open("file.txt", "x", encoding="utf-8") as f:
+        f.write(str(mylist))
 
+except FileExistsError:
+    print("file.txt already exists, exclusive creation aborted.")
 
- 
+with open("file.txt", "r") as f:
+    content = f.read()
+    mylist = eval(content)
+    print(type(mylist), mylist)
