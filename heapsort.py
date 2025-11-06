f2f9f28012d0865200a57574657ec8a4fb438d8d
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2025-11-06 09:01:48 -0500

    minor changes

--- heapsort.py
+++ heapsort.py
@@ -1,18 +1,9 @@
-
-# Usar misma lista predefinida
-arr = []
-
-with open("file.txt", "r") as f:
-    content = f.read()
-    arr = eval(content)
-    print(type(arr), arr)
-
 # Algoritmos de ordenamiento
 
 def heapify(A, n, i):
     max = i
-    left = 2*i+1
-    right = 2*i+2
+    left = 2 * i + 1
+    right = 2 * i + 2
 
     if (left < n and A[i] < A[left]):
         max = left
@@ -26,6 +17,7 @@ def heapify(A, n, i):
 
 def heapsort(A):
     n = len(A)
+
     for i in range(n, 0, -1):
         heapify(A, n, i)
 
@@ -34,6 +26,15 @@ def heapsort(A):
 
         heapify(A, i, 0)
 
+# Usar misma lista predefinida
+arr = []
+
+with open("file.txt", "r") as f:
+    content = f.read()
+    arr = eval(content)
+    print(type(arr), arr)
+
+
 # ejecutar algoritmo
 
 heapsort(arr)
@@ -41,9 +42,5 @@ heapsort(arr)
 print ("Sorted array is")
 print (arr)
 
-try:
-    with open("heapResult.txt", "x", encoding="utf-8") as f:
-        f.write(str(arr))
-
-except FileExistsError:
-    print("heapResult.txt already exists, exclusive creation aborted.")
+with open("heapResult.txt", "w", encoding="utf-8") as f:
+    f.write(str(arr))
